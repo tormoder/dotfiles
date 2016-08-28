38bc8f86bfdd584ff182682bfd8b4143078069ff
Tormod Erevik Lea <tormodlea@gmail.com>
2016-08-28 17:20:31 +0200

    bin: fix path in backup-pkg.sh

--- bin/bin/backup-pkg.sh
+++ bin/bin/backup-pkg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pacman -Qqe | grep -vx "$(pacman -Qqm)" > /storage/disk2/Backup/workstation/pkg.list
+pacman -Qqe | grep -vx "$(pacman -Qqm)" > /storage/disk2/d/backup/workstation/pkg.list
